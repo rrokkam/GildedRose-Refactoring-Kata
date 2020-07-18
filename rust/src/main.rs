be7334302f20e3b654cb3bba61df69711a36ee5f
rrokkam <rohithrokkam@gmail.com>
2020-07-18 16:13:24 -0700

    Further simplification for minimal example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,4 @@
 trait Tick {
-    fn tick(&mut self);
     fn days_remaining(&self) -> i32;
 }
 
@@ -9,40 +8,19 @@ impl PartialEq for dyn Tick {
     }
 }
 
-impl Eq for dyn Tick {}
-
-pub struct Item {
-    name: String,
+struct Item {
     days_remaining: i32,
-    quality: u32,
-    item: Option<Box<dyn Tick>>,
 }
 
 impl Tick for Item {
-    fn tick(&mut self) {
-        self.item.as_mut().unwrap().tick();
-    }
-
     fn days_remaining(&self) -> i32 {
         self.days_remaining
     }
 }
 
-impl Item {
-    pub fn new(name: impl AsRef<str>, days_remaining: i32, quality: u32) -> Item {
-        let name = name.as_ref().to_string();
-        Item {
-            name,
-            days_remaining,
-            quality,
-            item: None,
-        }
-    }
-}
-
 fn main() {
-    let item = Item::new("name", 5, 3);
-    let item2 = Item::new("blah", 3, 2);
+    let item = Item { days_remaining: 3 };
+    let item2 = Item { days_remaining: 3 };
     if item == item2 {
         println!("They're equal!")
     }
