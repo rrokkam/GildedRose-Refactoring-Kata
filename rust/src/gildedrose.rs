5a4e098a7bc8b9a237168b561b7662d2e6ecb45b
rrokkam <rohithrokkam@gmail.com>
2020-07-18 13:47:33 -0700

    Refactor backstage_tick

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -46,7 +46,7 @@ impl Item {
             return;
         }
         self.quality += 1;
-        if self.days_remaining < 0 && self.quality != 50 {
+        if self.days_remaining < 0 && self.quality < 50 {
             self.quality += 1;
         }
     }
@@ -54,22 +54,22 @@ impl Item {
     fn sulfuras_tick(&mut self) {}
 
     fn backstage_tick(&mut self) {
-        if self.quality < 50 {
-            self.quality += 1;
-
-            if self.days_remaining < 11 && self.quality < 50 {
-                self.quality += 1;
-            }
-
-            if self.days_remaining < 6 && self.quality < 50 {
-                self.quality += 1;
-            }
-        }
-
         self.days_remaining -= 1;
-
         if self.days_remaining < 0 {
             self.quality = 0;
+            return;
+        }
+
+        if self.quality < 50 {
+            self.quality += 1;
+        }
+
+        if self.days_remaining < 10 && self.quality < 50 {
+            self.quality += 1
+        }
+
+        if self.days_remaining < 5 && self.quality < 50 {
+            self.quality += 1
         }
     }
 }
